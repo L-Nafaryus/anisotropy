ce84f6837eb8bd125833dec9240d74d7be659396
L-Nafaryus <l.nafaryus@gmail.com>
2021-05-21 12:59:23 +0500

    Mod: permeability comparison

--- extra/theta-flowrate.py
+++ extra/theta-flowrate.py
@@ -21,6 +21,9 @@ if __name__ == "__main__":
     ]
     flowrate = [ [] for n in range(3) ]
 
+    #nu = 1e-06
+    #p = [1e-03, 0]
+
     for num, d in enumerate(directions):
         for t in theta:
             path = os.path.join(
@@ -37,29 +40,40 @@ if __name__ == "__main__":
             value = lastLine.replace(" ", "").replace("\n", "").split("\t")[1]
             flowrate[num].append(float(value))
 
+    k2, k3 = [], []
+
+    for n, _ in enumerate(flowrate[0]):
+        k2.append(2 * flowrate[1][n] / flowrate[0][n])
+        k3.append(2 * flowrate[2][n] / flowrate[0][n])
+
     
     plt.figure(1)
     
-    #plt.subplot(211)
-    line, = plt.plot(theta, flowrate[0], "o")
+    ax = plt.subplot(211)
+    line, = ax.plot(theta, flowrate[0], "o")
     line.set_label("[1, 0, 0]")
-    #plt.grid(True)
-    #plt.legend()
-    #plt.xlabel("theta")
-    #plt.ylabel("flowRate")
 
-    #plt.subplot(212)
-    line, = plt.plot(theta, flowrate[1], "o")
+    line, = ax.plot(theta, flowrate[1], "o")
     line.set_label("[0, 0, 1]")
-    #plt.grid(True)
-    #plt.xlabel("theta")
-    #plt.ylabel("flowRate")
 
-    #plt.subplot(213)
     line, = plt.plot(theta, flowrate[2], "o")
     line.set_label("[1, 1, 1]")
+    
+    ax.set_yscale("log")
+
     plt.legend()
     plt.grid(True)
     plt.xlabel("theta")
     plt.ylabel("flowRate")
+    
+    ax = plt.subplot(212)
+    line, = ax.plot(theta, k2, "o")
+    line.set_label("k2")
+    line, = ax.plot(theta, k3, "o")
+    line.set_label("k3")
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel("theta")
+    plt.ylabel("k")
+
     plt.show()
