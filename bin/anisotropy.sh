7ac40272ff04e2372e7b77ee6de910396c31e734
L-Nafaryus <l.nafaryus@gmail.com>
2021-05-31 17:29:23 +0500

    New: executable and bin location

--- bin/anisotropy.sh
+++ bin/anisotropy.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+cd ${DIR}
 
 if [ "$1" = "clean" ]; then
     git clean -fdx
-    exit 0
 
 elif [ "$1" = "init" ]; then
     python -m venv env
     source env/bin/activate
     python -m pip install --upgrade pip
-    python -m pip install requirements.txt
+    python -m pip install -r requirements.txt
+    deactivate
 
 elif [ "$1" = "run" ]; then
     source ${OPENFOAM}
+    source env/bin/activate
     python ${DIR}/anisotropy/anisotropy.py
+    deactivate
 
 fi
