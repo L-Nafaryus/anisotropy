4f528318dafc10775bdbd35375c53db29e0ccee4
L-Nafaryus <l.nafaryus@gmail.com>
2021-07-13 14:36:11 +0500

    Mod: db models

--- anisotropy/db.py
+++ anisotropy/db.py
@@ -8,12 +8,46 @@ class Structure(BaseModel):
     name = TextField()
     direction = TextField()
     theta = FloatField()
+    
+    r0 = FloatField()
+    L = FloatField()
+    radius = FloatField()
+    length = FloatField()
+    width = FloatField()
+    height = FloatField()
+    
+    fillets = FloatField()
+    path = TextField()
 
 class Mesh(BaseModel):
-    maxSize = FloatField()
-    minSize = FloatField()
+    structure = ForeignKeyField(Structure, backref = "meshes")
+
+    maxSize = FloatField() 
+    minSize = FloatField() 
+
+    fineness = IntegerField() 
+    growthRate = FloatField()
+    nbSegPerEdge = FloatField()
+    nbSegPerRadius = FloatField()
+    
     chordalErrorEnabled = BooleanField()
     chordalError = FloatField()
+    
+    secondOrder = BooleanField()
+    optimize = BooleanField()
+    quadAllowed = BooleanField()
+    useSurfaceCurvature = BooleanField()
+    fuseEdges = BooleanField()
+    checkChartBoundary = BooleanField()
+
+    viscousLayers = BooleanField()
+    thickness = FloatField()
+    numberOfLayers = IntegerField()
+    stretchFactor = FloatField()
+    isFacesToIgnore = BooleanField()
+    #facesToIgnore = ["inlet", "outlet"]
+    #faces = []
+    #extrusionMethod = ExtrusionMethod.SURF_OFFSET_SMOOTH
     calculationTime = TimeField()
-    structure = ForeignKeyField(Structure, backref = "mesh")
+
 
